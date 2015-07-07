6ac42b2b025dac6ff681ac2f8519cfe3b2191550
Ajinkya Kulkarni <quaintquid@gmail.com>
2015-07-07 17:53:16 +0530

    more balls

--- Assets/scripts/creator.cs
+++ Assets/scripts/creator.cs
@@ -5,11 +5,17 @@ public class creator : MonoBehaviour {
 	public power powerprefab;
 	// Use this for initialization
 	void Start () {
-		Vector3 pos1 = new Vector3 (3, 0, 4);
-		Vector3 pos2 = new Vector3 (3, 0, 1);
+		Vector3 pos1 = new Vector3 (Random.Range(-8,9), 0, Random.Range (-5, 5));
+		Vector3 pos2 = new Vector3 (Random.Range(-8,9), 0, Random.Range (-5, 5));
+		Vector3 pos3 = new Vector3 (Random.Range(-8,9), 0, Random.Range (-5, 5));
+		Vector3 pos4 = new Vector3 (Random.Range(-8,9), 0, Random.Range (-5, 5));
+		Vector3 pos5 = new Vector3 (Random.Range(-8,9), 0, Random.Range (-5, 5));
 
 		Instantiate (powerprefab, pos1, Quaternion.identity);
 		Instantiate (powerprefab, pos2, Quaternion.identity);
+		Instantiate (powerprefab, pos3, Quaternion.identity);
+		Instantiate (powerprefab, pos4, Quaternion.identity);
+		Instantiate (powerprefab, pos5, Quaternion.identity);
 
 
 	}
