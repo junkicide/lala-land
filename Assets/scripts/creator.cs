9d2426b857576a437ea44d377f5023bbe9419608
Ajinkya Kulkarni <quaintquid@gmail.com>
2015-07-07 15:51:01 +0530

    first commit - test

---
+++ Assets/scripts/creator.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class creator : MonoBehaviour {
+	public power powerprefab;
+	// Use this for initialization
+	void Start () {
+		Vector3 pos1 = new Vector3 (3, 0, 4);
+		Vector3 pos2 = new Vector3 (3, 0, 1);
+
+		Instantiate (powerprefab, pos1, Quaternion.identity);
+		Instantiate (powerprefab, pos2, Quaternion.identity);
+
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
---
+++ Assets/scripts/player.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class player : MonoBehaviour {
+	float sideways;
+	float forward;
+	// Use this for initialization
+	void Start () {
+
+
+
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+		float speed = 5.0f;
+		sideways = Input.GetAxisRaw("Horizontal");
+		forward = Input.GetAxisRaw("Vertical");
+		Vector3 movement = new Vector3 (sideways, 0, forward);
+
+		GetComponent<Rigidbody>().velocity = movement * speed;
+
+	}
+	void OnCollisionEnter (Collision other) {
+		if(other.gameObject.tag == "power") { 
+			other.transform.parent = transform;
+		}
+	}
+}
